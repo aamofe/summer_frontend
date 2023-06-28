7c98853a2b20503c21f400640841d136a5c7ce54
aamofe <111411075+aamofe@users.noreply.github.com>
2023-06-28 15:50:40 +0800

    初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+const LoginView = () => import('../views/LoginView.vue')
 const routes = [
   {
     path: '/',
@@ -14,6 +14,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView
   }
 ]
 
